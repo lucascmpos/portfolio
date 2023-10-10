b886f8439a34ca3a2225d8b4e3562a362e9a8f13
Lucas Campos <lucasoliveiracampos81@gmai.com>
2023-10-10 20:05:32 -0300

    Pequena alteração nas imagens

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,11 +3,15 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../styles/components/SliderImages.sass";
 import "../styles/components/Projects.sass";
+import projeto1 from '../src/img/projeto1.jpeg'
+import projeto2 from '../src/img/projeto2.jpeg'
+import projeto3 from '../src/img/projeto3.jpeg'
+
 
 const Projects = () => {
   const images = [
     {
-      src: "../src/img/projeto1.jpeg",
+      src: projeto1,
       caption: "Compass UOL Weekly Planner",
       description:
         "Feito em ReactJS, o usuário se cadastra, pode adicionar e remover tarefas da sua semana. Aplicação se conecta em um banco de dados CRUD  para validação de usuário pela SwaggerAPI. Também foi integrada a WeatherAPI para clima da respectiva cidade. (O projeto foi desenvolvido com um banco de dados da Compass UOL durante meu estágio, onde hoje em dia foi excluído. No entanto, estarei disponibilizando uma demonstração em vídeo no meu LinkedIn gravada na época).",
@@ -15,14 +19,14 @@ const Projects = () => {
       href2: "https://github.com/lucascmpos/Projeto-Compass-VIII"
     },
     {
-      src: "../src/img/projeto2.jpeg",
+      src: projeto2,
       caption: "Upload.AI",
       description: "Feito em ReactTS e NodeTS, o usuário sobe um vídeo na plataforma e a IA através do Chat-GPT 3.5, transcreve em forma de texto, podendo alterar seus comandos como quiser. (Depois de um certo tempo, a transcrição pode não funcionar mais, devido ao limite financeiro da plataforma OpenAI).",
       href1: "https://uploadai.netlify.app/",
       href2: "https://github.com/lucascmpos/NLW-IA"
     },
     {
-      src: "../src/img/projeto3.jpeg",
+      src: projeto3,
       caption: "MonFi",
       description: "Feito em ReactJS, o usuário pode consultar qualquer filme através de pesquisa, recebendo informações sobre o filme. Integrado com a API da The Movie DB.",
       href1: "https://monfi.netlify.app/",
